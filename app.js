1a3c04444453d9075c08973515e76d88ad0b1393
Paul Cook <paul.cook@adtechzen.com>
2014-02-09 19:49:34 +0000

    v1

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express'),
     app = express();
 //    minify = require('express-minify');
 
-var SERVER= { PORT: 80,
-			  maxAge: 60};
+var SERVER= { PORT: 8000,
+			  maxAge: 0};
 
 /**
  * Middleware
